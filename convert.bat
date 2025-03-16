5ea6a5b9388e41428a0f007898f5ee2b2849eacc
Tomáš Gál <tomas.gal@upcmail.sk>
2025-03-16 13:40:21 +0100

    Update convert.bat

--- convert.bat
+++ convert.bat
@@ -1,2 +1,10 @@
-ffmpeg -i %1 -c:v copy -c:a libmp3lame -b:a 192k -ac 2 -af "volume=3" yt.mp4
+setlocal enabledelayedexpansion
+cd /d "%~dp0"
+
+for %%F in (*.mp4) do (
+ffmpeg -i "%%F" -c:v copy -c:a libmp3lame -b:a 192k -ac 2 -af "volume=3" yt.mp4
 ffmpeg -i yt.mp4 -vf "scale=1280:720" -c:v libx264 -crf 23 -preset medium -c:a copy spotify.mp4
+exit /b
+)
+
+endlocal
